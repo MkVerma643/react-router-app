968a5cf50175d09337a6810875791033b4e88861
Mukesh Verma <mukeshverma643@gmail.com>
2022-09-13 12:34:33 +0530

    Uses React Router, Link, Routes, Route, UseParams, Outlet, navigate

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/myapps" element={<Navigate replace to="/learn" />}></Route>
+      <Route path="/learn" element={<Learn />}>
+        <Route path="courses" element={<Courses />}>
+          <Route path=":courseid" element={<CourseId />} />
+        </Route>
+        <Route path="bundles" element={<Bundles />} />
+      </Route>
     </Routes>
   </Router>
 );
@@ -21,6 +36,60 @@ function Home() {
       <h1>Home</h1>
       <div>
         <h2>Home Router</h2>
+      </div>
+    </div>
+  );
+}
+
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <div>
+        <h2>Learn List</h2>
+        <Link className="btn btn-success" to="/learn/courses">
+          Courses
+        </Link>
+        {" | "}
+        <Link className="btn btn-primary" to="/learn/bundles">
+          Bundle
+        </Link>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+}
+
+function Courses() {
+  return (
+    <div>
+      <h1>Courses</h1>
+      <div>
+        <h4>Courses List</h4>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+}
+
+function Bundles() {
+  return (
+    <div>
+      <h1>Bundles</h1>
+      <div>
+        <h4>Bundles Card</h4>
+      </div>
+    </div>
+  );
+}
+
+function CourseId() {
+  const { courseid } = useParams();
+  return (
+    <div>
+      <h1>URL Param is: {courseid}</h1>
+      <div>
+        <h4>Bundles Card</h4>
       </div>
     </div>
   );
